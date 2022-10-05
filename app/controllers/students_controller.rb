f745d26079f113a4e61b9daab66f22d929e3420b
Mariusz <mariuszbia@yahoo.com>
2022-10-04 22:48:06 -0500

    Added viewing students and adding.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,9 @@ class StudentsController < ApplicationController
         students = User.where(role: 'student')
         render json: students
     end
+
+    def create
+        student = User.create( first_name: params[ :first_name ], last_name: params[ :last_name ], email: params[ :email ], role: 'student' );
+        render json: student, status: :created
+    end
 end
